8819a60335f6d6abef7731e1b66bb0aa2fcaa2c2
jonexo7official <jonexo7official@gmail.com>
2026-06-15 11:09:08 +0200

    mi web jonexo

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="w-screen h-screen bg-black m-0 p-0">
+      <img
+        src="/jonexo-overlay.webp"
+        alt="Jonexo70Oficial"
+        className="w-full h-full object-cover object-center select-none"
+        draggable={false}
+      />
+    </main>
+  );
+}
